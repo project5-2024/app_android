dc697105639bc50fecb0133a22d1db80a508bdeb
jeffh90 <jeffhalley420@gmail.com>
2024-10-20 19:22:09 +0100

    add extra space

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -14,6 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var usernameInput: EditText
     lateinit var passwordInput: EditText
