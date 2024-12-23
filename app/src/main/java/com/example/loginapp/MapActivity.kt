49061a9c97bb0818cd6e84663d54ebefe5a770c4
igor <20102236@mail.wit.ie>
2024-12-23 21:12:55 +0000

    added multiplier array

--- app/src/main/java/com/example/loginapp/MapActivity.kt
+++ app/src/main/java/com/example/loginapp/MapActivity.kt
@@ -25,13 +25,18 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import org.json.JSONArray
 import org.json.JSONObject
+import org.json.*
+import java.util.Objects
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
-
+    private var mylocation = LatLng(0.0,0.0);
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var googleMap: GoogleMap
     private val handler = Handler(Looper.getMainLooper())
     private val updateInterval: Long = 60000 // 1 minute in milliseconds
+    private val distances = mutableMapOf<String, Float>()
+    private val preferences = mutableMapOf<String, Float>("music" to 0f, "food" to 0f, "travel" to 0f, "movies" to 0f, "technology" to 0f, "fitness" to 0f, "gaming" to 0f, "books" to 0f, "fashion" to 0f)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,7 +75,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 val currentLatLng = LatLng(location.latitude, location.longitude)
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
                 googleMap.addMarker(MarkerOptions().position(currentLatLng).title("You are here"))
-
+                mylocation = currentLatLng
                 loadNearbyShops(location) // Load nearby places
             } else {
                 Toast.makeText(this, "Unable to fetch location", Toast.LENGTH_SHORT).show()
@@ -80,15 +85,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun loadNearbyShops(location: Location) {
         val radius = 1000 // 1 km radius
-        val apiKey = ""
+        val apiKey = "AIzaSyDMOr-Z3XYTZC8rt9DFopMioNAimNBav5M"
 
         val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
-                "location=${location.latitude},${location.longitude}&radius=$radius&type=store&key=$apiKey"
+                "location=${location.latitude},${location.longitude}&radius=$radius&key=$apiKey"
 
         val requestQueue = Volley.newRequestQueue(this)
         val stringRequest = StringRequest(Request.Method.GET, url,
             Response.Listener<String> { response ->
-                Log.d("MapActivity", "Places API Response: $response")
                 try {
                     val results = JSONObject(response).getJSONArray("results")
                     if (results.length() == 0) {
@@ -96,14 +100,80 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                         return@Listener
                     }
 
+                    val poiCounter = mutableMapOf<String, Int>()
+
                     for (i in 0 until results.length()) {
                         val place = results.getJSONObject(i)
                         val name = place.getString("name")
                         val lat = place.getJSONObject("geometry").getJSONObject("location").getDouble("lat")
                         val lng = place.getJSONObject("geometry").getJSONObject("location").getDouble("lng")
                         val latLng = LatLng(lat, lng)
+
+                        // Calculate the distance
+                        val myloc = Location("mylocation").apply {
+                            latitude = location.latitude
+                            longitude = location.longitude
+                        }
+                        val loc = Location("destination").apply {
+                            latitude = latLng.latitude
+                            longitude = latLng.longitude
+                        }
+                        val distance = myloc.distanceTo(loc)
+
                         googleMap.addMarker(MarkerOptions().position(latLng).title(name))
+
+                        val types = place.getJSONArray("types")
+                        for (j in 0 until types.length()) {
+                            val type = types.getString(j)
+
+                            val category = when {
+                                type.contains("lodging") || type.contains("travel_agency") || type.contains("airport") ||
+                                        type.contains("bus_station") || type.contains("train_station") || type.contains("subway_station") ||
+                                        type.contains("taxi_stand") || type.contains("transit_station") -> "travel"
+
+                                type.contains("gym") || type.contains("spa") || type.contains("health") ||
+                                        type.contains("stadium") || type.contains("park") -> "fitness"
+
+                                type.contains("movie_theater") || type.contains("drive_in_theater") -> "movies"
+
+                                type.contains("restaurant") || type.contains("cafe") || type.contains("bakery") ||
+                                        type.contains("bar") || type.contains("food") || type.contains("meal_takeaway") ||
+                                        type.contains("grocery_or_supermarket") || type.contains("liquor_store") ||
+                                        type.contains("convenience_store") -> "food"
+
+                                type.contains("stadium") || type.contains("sports_complex") || type.contains("bowling_alley") ||
+                                        type.contains("ice_skating_rink") || type.contains("golf_course") || type.contains("park") -> "sports"
+
+                                type.contains("shopping_mall") || type.contains("clothing_store") || type.contains("shoe_store") ||
+                                        type.contains("jewelry_store") || type.contains("department_store") || type.contains("boutique") -> "fashion"
+
+                                type.contains("electronics_store") || type.contains("hardware_store") || type.contains("computer_store") ||
+                                        type.contains("phone_store") || type.contains("home_goods_store") -> "technology"
+
+                                type.contains("amusement_park") || type.contains("arcade") || type.contains("casino") ||
+                                        type.contains("escape_room") -> "gaming"
+
+                                type.contains("art_gallery") || type.contains("museum") || type.contains("music_venue") ||
+                                        type.contains("night_club") || type.contains("theater") -> "music"
+
+                                type.contains("library") || type.contains("book_store") || type.contains("university") || type.contains("school") -> "books"
+
+                                else -> null
+                            }
+
+                            if (category != null) {
+                                poiCounter[category] = poiCounter.getOrDefault(category, 0) + 1
+                            }
+                        }
                     }
+
+                    // Update preferences
+                    for ((category, count) in poiCounter) {
+                        val multiplier = count.toFloat() / radius
+                        preferences[category] = multiplier
+                        Log.d("type count", "Category: $category, Count: $count, Multiplier: $multiplier")
+                    }
+                    Log.d("Preferences multiplier", preferences.toString())
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
