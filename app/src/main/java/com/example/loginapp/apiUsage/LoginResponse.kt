f6425bae9799497d7ede4af3d26113b8e60d3f61
Igor Kapusniak <114166214+igorkapusniak0@users.noreply.github.com>
2025-01-03 18:52:55 +0000

    Update LoginResponse.kt

--- app/src/main/java/com/example/loginapp/apiUsage/LoginResponse.kt
+++ app/src/main/java/com/example/loginapp/apiUsage/LoginResponse.kt
@@ -3,7 +3,6 @@ package com.example.loginapp.apiUsage
 data class LoginResponse(
     val userId: String,
     val token: String,
-    // Other fields as per your API response
 ) {
     override fun toString(): String {
         return "LoginResponse(userId='$userId', token='$token')"
